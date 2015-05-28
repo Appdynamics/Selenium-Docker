2239b1de1c104f60c208aa69753ba3c06f43e28d
Jeff Morgan <jeff.morgan@appdynamics.com>
2015-05-28 00:08:35 +0000

    Updated .cfg files so Docker can run 1 app per container

--- Docker/startup.sh
+++ Docker/startup.sh
@@ -4,21 +4,26 @@
 
 # Set EC2 Region variable
 source /appdynamics/env.sh && sed -i "s/EC2_REGION/${EC2_REGION}/g" /appdynamics/SeleniumDemoLoad/seleniumload.sh
+source /appdynamics/env.sh && sed -i "s/APP/${APP}/g" /appdynamics/SeleniumDemoLoad/seleniumload.sh
 
 # Input Ecomm IPs
-source /appdynamics/env.sh && find /appdynamics/SeleniumDemoLoad/config/ -type f -name "*.cfg" -exec sed -i'' -e "s/ecommerce1/${ECOMM1}/g" {} +
-source /appdynamics/env.sh && find /appdynamics/SeleniumDemoLoad/config/ -type f -name "*.cfg" -exec sed -i'' -e "s/ecommerce2/${ECOMM2}/g" {} +
-source /appdynamics/env.sh && find /appdynamics/SeleniumDemoLoad/config/ -type f -name "*.cfg" -exec sed -i'' -e "s/ecommercestaging/${ECOMMSTAGING}/g" {} +
+#source /appdynamics/env.sh && for i in $(ls /appdynamics/SeleniumDemoLoad/config/*.cfg); do sed -i "s/ecommercestaging/${ECOMMSTAGING}/g" $i; done
+#source /appdynamics/env.sh && for i in $(ls /appdynamics/SeleniumDemoLoad/config/*.cfg); do sed -i "s/bundystaging/${BUNDYSTAGING}/g" $i; done
+#source /appdynamics/env.sh && for i in $(ls /appdynamics/SeleniumDemoLoad/config/*.cfg); do sed -i "s/moviestaging/${MOVIESTAGING}/g" $i; done
+
+source /appdynamics/env.sh && find /appdynamics/SeleniumDemoLoad/config/ -type f -name "*.cfg" -exec sed -i'' -e "s/ecommerce/${ECOMM}/g" {} +
+#source /appdynamics/env.sh && find /appdynamics/SeleniumDemoLoad/config/ -type f -name "*.cfg" -exec sed -i'' -e "s/ecommerce2/${ECOMM2}/g" {} +
+#source /appdynamics/env.sh && find /appdynamics/SeleniumDemoLoad/config/ -type f -name "*.cfg" -exec sed -i'' -e "s/ecommercestaging/${ECOMMSTAGING}/g" {} +
 
 # Input Bundy IPs
-source /appdynamics/env.sh && find /appdynamics/SeleniumDemoLoad/config/ -type f -name "*.cfg" -exec sed -i'' -e "s/bundy1/${BUNDY1}/g" {} +
-source /appdynamics/env.sh && find /appdynamics/SeleniumDemoLoad/config/ -type f -name "*.cfg" -exec sed -i'' -e "s/bundy2/${BUNDY2}/g" {} +
-source /appdynamics/env.sh && find /appdynamics/SeleniumDemoLoad/config/ -type f -name "*.cfg" -exec sed -i'' -e "s/bundystaging/${BUNDYSTAGING}/g" {} +
+source /appdynamics/env.sh && find /appdynamics/SeleniumDemoLoad/config/ -type f -name "*.cfg" -exec sed -i'' -e "s/bundy/${BUNDY}/g" {} +
+#source /appdynamics/env.sh && find /appdynamics/SeleniumDemoLoad/config/ -type f -name "*.cfg" -exec sed -i'' -e "s/bundy2/${BUNDY2}/g" {} +
+#source /appdynamics/env.sh && find /appdynamics/SeleniumDemoLoad/config/ -type f -name "*.cfg" -exec sed -i'' -e "s/bundystaging/${BUNDYSTAGING}/g" {} +
 
 # Input MovieTix IPs
-source /appdynamics/env.sh && find /appdynamics/SeleniumDemoLoad/config/ -type f -name "*.cfg" -exec sed -i'' -e "s/movietix1/${MOVIE1}/g" {} +
-source /appdynamics/env.sh && find /appdynamics/SeleniumDemoLoad/config/ -type f -name "*.cfg" -exec sed -i'' -e "s/movietix2/${MOVIE2}/g" {} +
-source /appdynamics/env.sh && find /appdynamics/SeleniumDemoLoad/config/ -type f -name "*.cfg" -exec sed -i'' -e "s/movietixstaging/${MOVIESTAGING}/g" {} +
+source /appdynamics/env.sh && find /appdynamics/SeleniumDemoLoad/config/ -type f -name "*.cfg" -exec sed -i'' -e "s/movietix/${MOVIE}/g" {} +
+#source /appdynamics/env.sh && find /appdynamics/SeleniumDemoLoad/config/ -type f -name "*.cfg" -exec sed -i'' -e "s/movietix2/${MOVIE2}/g" {} +
+#source /appdynamics/env.sh && find /appdynamics/SeleniumDemoLoad/config/ -type f -name "*.cfg" -exec sed -i'' -e "s/movietixstaging/${MOVIESTAGING}/g" {} +
 
 # Start MachineAgent
 #su - appdynamics -c "source /appdynamics/env.sh && sed -i 's/AGENT_OPTIONS -Dappdynamics.agent.uniqueHostId=/AGENT_OPTIONS -Dappdynamics.controller.hostName=${CONTROLLER} -Dappdynamics.controller.port=${APPD_PORT} -Dappdynamics.agent.uniqueHostId=/g' /appdynamics/MachineAgent/startMachineAgent.sh"
